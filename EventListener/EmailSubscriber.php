78493bb3bbf9045fdc0ae60a56f4203540dc95d5
filchakov <filchakov.denis@gmail.com>
2018-03-27 12:03:46 +0300

    fix running level for plugin

--- EventListener/EmailSubscriber.php
+++ EventListener/EmailSubscriber.php
@@ -17,47 +17,51 @@ class EmailSubscriber extends CommonSubscriber
     public static function getSubscribedEvents()
     {
         return array(
-            EmailEvents::EMAIL_ON_SEND    => array('onEmailGenerate', 0),
-            EmailEvents::EMAIL_ON_DISPLAY => array('onEmailGenerate', 0)
+            EmailEvents::EMAIL_ON_SEND    => array('onEmailGenerate', 10000),
+            EmailEvents::EMAIL_ON_DISPLAY => array('onEmailGenerate', 10000)
         );
     }
 
     public function onEmailGenerate(EmailSendEvent $event){
 
-        $dom = new DomDocument();
+        try {
+            $dom = new DomDocument();
 
-        $dom->loadHTML($event->getContent());
+            $dom->loadHTML($event->getContent());
 
-        $finder = new DomXPath($dom);
+            $finder = new DomXPath($dom);
 
-        $cl_job_item = 'rss-job-item';
-        $cl_title = "rss-title";
-        $cl_description = "rss-description";
+            $cl_job_item = 'rss-job-item';
+            $cl_title = "rss-title";
+            $cl_description = "rss-description";
 
-        $item_job = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_job_item ')]");
-        $count_jobs = $item_job->length;
+            $item_job = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_job_item ')]");
+            $count_jobs = $item_job->length;
 
-        $xml_feed_url = $item_job->item(0)->getAttribute('data-rss-url');
-        $xml_feed_data = $this->getFeedData($xml_feed_url);
+            if(!empty($count_jobs)){
+                $xml_feed_url = $item_job->item(0)->getAttribute('data-rss-url');
+                $xml_feed_data = $this->getFeedData($xml_feed_url);
 
-        for ($i = 0; $i < $count_jobs; $i++){
+                for ($i = 0; $i < $count_jobs; $i++){
 
-            $item_from_rss_feed = current($xml_feed_data);
+                    $item_from_rss_feed = current($xml_feed_data);
 
-            $rss_title = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_title ')]")->item($i);
-            $rss_title->textContent = $item_from_rss_feed->title;
-            $rss_title->setAttribute('href', $item_from_rss_feed->link);
+                    $rss_title = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_title ')]")->item($i);
+                    $rss_title->textContent = $item_from_rss_feed->title;
+                    $rss_title->setAttribute('href', $item_from_rss_feed->link);
 
+                    $rss_description = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_description ')]")->item($i);
+                    $rss_description->textContent = strip_tags($item_from_rss_feed->description);
 
+                    next($xml_feed_data);
+                }
+            }
 
-            $rss_description = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $cl_description ')]")->item($i);
-            $rss_description->textContent = $item_from_rss_feed->description;
+            $event->setContent($finder->document->saveHTML());
 
-            next($xml_feed_data);
+        } catch (\Exception $e){
+
         }
-
-        $event->setContent($finder->document->saveHTML());
-
     }
 
     /**
